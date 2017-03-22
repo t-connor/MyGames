9db76121588b527dd1f12ec6237b3b0b003f7434
t-connor <connor_haw@foxmail.com>
2017-03-22 10:08:44 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,37 +3,62 @@ import re
 import code
 import urllib2
 
-city = raw_input ('which city you want to know:\n')
+def check_weather():
 
-url = ('http://flash.weather.com.cn/wmaps/xml/'+ str(city) +'.xml')
-content = urllib2.urlopen(url).read()
+    try:
+        compare_temp1 = r'tem1="([-]?\d+)"'
+        compare_temp2 = r'tem2="([-]?\d+)"'
+        compare_tempNow = r'temNow="([-]?\d+)"'
+        comoare_weather = r'stateDetailed="(.+?)"'
+        compare_cityname = 'centername="(.+?)"'
+        compare_windState = 'windState="(.+?)"'
 
-f = file('weather.xml', 'w')
-f.write(content)
-f.close()
+        temp1 = re.findall(compare_temp1, content)
+        temp2 = re.findall(compare_temp2, content)
+        tempNow = re.findall(compare_tempNow, content)
+        weather = re.findall(comoare_weather, content)
+        windState = re.findall(compare_windState, content)
+        cityname = re.findall(compare_cityname, content)
+        
+        ##print temp1
+        ##print temp2
+            
+    except:
+        print 'input error'
+        
+    if len(cityname) == 0:
+        print 'input error'
+        return
 
+    print '\n天气播报：\n'
+            
+    for i in range(len(cityname)):
+        print cityname[i].decode('utf-8').encode('gb2312'),
+        print '\t',
+        print weather[i].decode('utf-8').encode('gb2312'),
+        print '\t',
 
-print content
+        if int(temp1[i]) >= int(temp2[i]): ##sometimes temp1<temp2
+            print '%s-%s ℃\t' % (temp2[i], temp1[i]),
+        else:
+            print '%s-%s ℃\t' % (temp2[i], temp2[i]),
+        print '现在室外温度' + tempNow[i] + '℃\t',
+        ##print windState[i].decode('utf-8').encode('gb2312')
+        print windState[i]
+    print
+        
 
-try:
+if __name__ == '__main__':
+    
+    city = raw_input ('which city you want to know(ues PINYIN):\n')
+    newCity = city.lower()
 
-    compare_num = r'\d+'
-    comoare_weather = r'stateDetailed="(.+?)"'
-    compare_cityname = 'cityname="(.+?)"'
+    url = ('http://flash.weather.com.cn/wmaps/xml/'+ str(newCity) +'.xml')
+    content = urllib2.urlopen(url).read()
 
-    num = re.findall(compare_num, content)
-    weather = re.findall(comoare_weather, content)
-    cityname = re.findall(compare_cityname, content)
-except:
-    print 'input error'
-##print isinstance(weather[0], unicode)
+    ##f = file('weather.xml', 'w')
+    ##f.write(content)
+    ##f.close()
 
-
-print '天气播报：'
-print cityname[0].decode('utf-8').encode('gb2312') 
-print weather[0].decode('utf-8').encode('gb2312') 
-print '%s-%s ℃' % (num[-7], num[-9])   ##酱紫不行啊~~
-print '现在室外温度' + num[-6] + '℃'
-print u'监测时间' + '%s:%s' % (num[-3], num[-2])
-
-
+    print content
+    check_weather()
